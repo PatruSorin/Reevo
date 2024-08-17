b485b360c9ff2dd80adf6b076b63a86048ad1b1f
Patru Sorin <patrusorin95@gmail.com>
2024-08-17 13:01:30 +0300

    CLI Calculator

---
+++ CLI_Calculator/app/Console/Commands/calculator.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class calculator extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:calculator {first_value} {operator} {second_value?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+    
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $operator = $this->argument("operator");
+        $first_value = $this->argument("first_value");
+        $second_value = $this->argument("second_value");
+
+        if(!is_numeric($first_value)) {
+            $this->error("First variable is not a number");
+            return;
+        }
+
+        switch ($operator) {
+            case '+':
+                if($second_value == null || !is_numeric($second_value)) {
+                    $this->error("This operation requires two values");
+                    return;
+                }
+                $this->line($first_value + $second_value);
+                break;
+            case '-':
+                if($second_value == null || !is_numeric($second_value)) {
+                    $this->error("This operation requires two values");
+                    return;
+                }
+                $this->line($first_value - $second_value);
+                break;
+            case '*':
+                if($second_value == null || !is_numeric($second_value)) {
+                    $this->error("This operation requires two values");
+                    return;
+                }
+                $this->line($first_value * $second_value);
+                break;
+            case '/':
+                if($second_value == null || !is_numeric($second_value)) {
+                    $this->error("This operation requires two values");
+                    return;
+                }
+
+                if($second_value == 0) {
+                    $this->error("Cannot divide by 0");
+                    return;
+                }
+
+                $this->line($first_value / $second_value);
+                break;
+            case 'sqrt':
+                $this->line(sqrt($first_value)); 
+                break;
+                
+            default:
+                $this->fail('Unknown operator');
+                break;
+        }
+    }
+}
